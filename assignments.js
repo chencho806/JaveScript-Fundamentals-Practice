f250f5fd1a89425b4354debfa605cb9a7241eae1
“coreyalvarez” <coreyalvarez1989@gmail.com>
2022-02-06 20:13:34 -0600

    first commit

---
+++ assignments.js
@@ -0,0 +1,36 @@
+// let x = 10 + 5;
+// x += 10;
+// x ++;
+// console.log(x >= 3);
+
+
+// let x, y;
+
+// x = y = 25 - 10 - 5;
+
+// console.log(x, y)
+
+// let massMark = 78
+// let heightMark = 1.69
+// let massJohn = 105
+// let heightJohn = 1.95
+
+// const BMIMark = massMark / heightMark **2;
+// const BMIJohn = massJohn / heightJohn **2;
+
+// const markBMIHigher = BMIMark > BMIJohn;
+
+// console.log(BMIMark)
+// console.log(BMIJohn)
+// console.log(markBMIHigher)
+
+
+const firstName = 'Chencho';
+const job = 'Developer'
+const birthYear = 1989;
+const year = 2022;
+
+
+const chencho = 'I\'m ' + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+
+console.log(chencho)
---
+++ script.js
@@ -0,0 +1,10 @@
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Chencho') 
+// console.log(32)
+
+
+// let firstName = "Chencho";
+
+// console.log(firstName)
